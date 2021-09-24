b0de83118b31b1e425b82a2cc4b53ed6637fa857
Mayu008 <kadam.ms@somaiya.edu>
2021-09-24 16:43:46 +0530

    Gihtub-Jenkins Integrations

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main( String []args ) {
+        int num1=Integer.parseInt(args[0]);
+        int num2=Integer.parseInt(args[1]);
+        System.out.println(num2);
+        for(int i=0;i<num1;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+    }
+}
