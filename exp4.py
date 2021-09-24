80efbc903ceb08706ebaa90e1c5f9b2d6e53513c
Mayu008 <kadam.ms@somaiya.edu>
2021-09-24 17:36:18 +0530

    Modified code Jenkins Integrations

--- exp4.py
+++ exp4.py
@@ -1,1 +1,4 @@
-print("Hello")
+# Program to add to number
+n1=int(input())
+n2=int(input())
+print(n1+n2)
---
+++ pattern.py
@@ -0,0 +1,14 @@
+
+
+# Simple C++ program to display "Hello World"
+
+count=1
+n1=4
+n2=4
+count2=0
+for i in range(0,n1):
+    for j in range(0,count2):
+        print(count,end=" ")
+        count+=1
+    count2+=1
+    print("\n")
