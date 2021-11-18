2325d62b221e3136db9b6aa36d507259da36a12a
unknown <kadammayuresh199@gmail.com>
2021-11-19 00:31:06 +0530

    The files to be added to github

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,10 @@
 public class HelloWorld {
-    public static void main( String []args ) {
-        int num1=Integer.parseInt(args[0]);
-        int num2=Integer.parseInt(args[1]);
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
         System.out.println(num2);
-        for(int i=0;i<num1;i++)
-        {
-            for(int j=0;j<=i;j++)
-            {
+        for (int i = 0; i < num1; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println("\n");
--- exp4.py
+++
@@ -1,4 +0,0 @@
-# Program to add to number
-n1=int(input())
-n2=int(input())
-print(n1+n2)
